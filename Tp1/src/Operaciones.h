d5c1235f8adcc855cd591369b53c9cb337691fca
Santiago <luquez.santiago@gmail.com>
2021-04-10 12:34:56 -0300

    Tp1 con comentarios agregados a las funciones con doxygen

--- Tp1/src/Operaciones.h
+++ Tp1/src/Operaciones.h
@@ -13,22 +13,55 @@
 #define OPERACIONES_H_
 #define TAM_BUFFER 1000
 
-int ValidarNumeroFloat(char* pData);
 
-float PedirFloat(char* MSJ, char* ERROR_MSJ);
-
-void MostrarNumero(int numeroIngresado);
-
+/**
+ * @fn float SumarOperadores(float, float)
+ * @brief Realiza la operacion de sumar con los operadores de la variable x e y ingresados previamente.
+ *
+ * @param x: Es cualquier numero tipo float pertenenciente a R
+ * @param y: Es cualquier numero tipo float pertenenciente a R
+ * @return El valor sumado
+ */
 float SumarOperadores(float x , float y);
 
+/**
+ * @fn float RestarOperadores(float, float)
+ * @brief Realiza la operacion de restar con los operadores de la variable x e y ingresados previamente.
+ *
+ * @param x: Es cualquier numero tipo float pertenenciente a R
+ * @param y: Es cualquier numero tipo float pertenenciente a R
+ * @return El valor restado
+ */
 float RestarOperadores (float x, float y);
+
+/**
+ * @fn float DividirOperadores(float, float)
+ * @brief Realiza la operacion de dividir con los operadores de la variable x e y ingresados previamente.
+ *
+ * @param x: Es cualquier numero tipo float pertenenciente a R
+ * @param y: Es cualquier numero tipo float pertenenciente a R, excepto el 0
+ * @return El valor producto de la dividion de los factores
+ */
 
 float DividirOperadores(float x, float y);
 
+/**
+ * @fn float MultiplicarOperadores(float, float)
+ * @brief Realiza la operacion de multiplicar con los operadores de la variable x e y ingresados previamente.
+ *
+ * @param x: Es cualquier numero tipo float pertenenciente a R
+ * @param y: Es cualquier numero tipo float pertenenciente a R
+ * @return El valor multiplicado por los factores
+ */
 float MultiplicarOperadores(float x, float y);
 
-int ValidarEntero(float x);
-
+/**
+ * @fn int CalcularFactorial(int)
+ * @brief Realiza la operacion de calcular factorial
+ *
+ * @param x: Es un numero positivo y entero.
+ * @return El valor de haber realizado el factorial de un numero
+ */
 int CalcularFactorial(int x);
 
 #endif /* OPERACIONES_H_ */
--- Tp1/src/Tp1_mejorado.c
+++ Tp1/src/Tp1_mejorado.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #include "Operaciones.h"
+#include "Herramientas.h"
 
 int main(void) {
 
